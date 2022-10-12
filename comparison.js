1fb26d4ff7d7da5f2e1ae076525ffc478030dc4a
jradnan <mohd.yasin085208@gmail.com>
2022-10-13 02:45:25 +0600

    condition

---
+++ comparison.js
@@ -0,0 +1,13 @@
+var samsungTablet = 230000;
+var myBudget = 10000;
+ if(myBudget > samsungTablet){
+    console.log('I will Buy');
+ }else{
+    console.log('I will not buy');
+ }
+
+ if (condition) {
+    
+ } else {
+    
+ }
