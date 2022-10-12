e3ec2bd51b105e96213b5ba77864e135a1647792
jradnan <mohd.yasin085208@gmail.com>
2022-10-12 23:43:45 +0600

    array / indexof

---
+++ array.js
@@ -0,0 +1,3 @@
+var food = ['apple', 'bananas', 'orange', 'mango',   ];
+console.log(food);
+console.log(food.length);
---
+++ arrayIndex.js
@@ -0,0 +1,7 @@
+var fruit = ['apple', 'banana', 'pineapple', 'mango', 'jackfruit'];
+var fruitCount = fruit.indexOf('mango')
+
+var fruitIndex = fruit[4];
+
+console.log(fruitIndex);
+console.log(fruitCount);
