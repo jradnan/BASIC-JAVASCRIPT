8fa0834d2ac9a3199eef759a8bf540ef6862d113
jradnan <mohd.yasin085208@gmail.com>
2022-10-12 19:01:20 +0600

    javascript parseFloat and parseInt

---
+++ script.js
@@ -0,0 +1,13 @@
+var eggPrice = "3.66";
+var applePrice = "4.99";
+var totall = eggPrice +applePrice;
+
+var egg = parseFloat(eggPrice);
+var apple = parseFloat(applePrice);
+var allPrice = egg +apple;
+
+
+var egg2 = parseInt(eggPrice);
+var apple2= parseInt(applePrice);
+var everything = egg2 + apple2;
+console.log(everything);
