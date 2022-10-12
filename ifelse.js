0629962ec1f22c321097f04e93070bc496e26b07
jradnan <mohd.yasin085208@gmail.com>
2022-10-12 22:43:23 +0600

    purno opurno

---
+++ ifelse.js
@@ -0,0 +1,24 @@
+var alia = 95;
+var dalia = 66;
+var salia = 80;
+var malia = 59;
+var lilia = 47;
+var jalia = 77;
+var total = alia , dalia , salia, malia, lilia, jalia;
+
+
+if(total < 50){
+ console.log('grade G');
+}else if(total < 70){
+    console.log('grade C');
+
+}else if(total < 80){
+    console.log('grade B');
+
+}else if(total < 90){
+    console.log('grade A');
+
+}else if(total < 100){
+    console.log('grade A+');
+
+}
