aafc43d1478b606d3e0bf4cc20ddca2c16d8b507
jradnan <mohd.yasin085208@gmail.com>
2022-10-14 04:21:44 +0600

    for loop

--- while-loop.js
+++ while-loop.js
@@ -1,6 +1,19 @@
 var beton = 0;
 
 while(beton<9){
+    console.log(beton);
     console.log('Sir, please amr ei month er beton den');
+
     beton++;
+}
+
+
+var number = 1;
+while(number <= 10){
+    console.log(number);
+ number++;
+}
+
+for(var beton =0; beton < 9; beton++){
+    console.log('bhai please amr beton diye den');
 }
