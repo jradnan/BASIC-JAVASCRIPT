4d97e5fd3260c0c850ae8484398e132c267b0b68
jradnan <mohd.yasin085208@gmail.com>
2023-02-27 11:26:43 +0600

    objectkeysvaluesa

---
+++ ES6/advanceObj.js
@@ -0,0 +1,16 @@
+const student = {
+    name: "Adnan",
+    money: 5000,
+    study: "Web Programming",
+    subjects: ['wordpress', 'javascript', 'React', 'mongo', 'node', 'front end', 'full stack'],
+    exam: function () {
+        return this.name + 'participating in an exam'
+    }
+};
+
+
+const bottle = {color:"yellow", price : 50, isCleaned:true, capacity:1};
+const keys = Object.keys(bottle);
+const values = Object.values(bottle)
+console.log(keys);
+console.log(values);
