4e2cb9633087f07348772b733a70461cfd8d053d
jradnan <mohd.yasin085208@gmail.com>
2023-02-27 10:23:19 +0600

    advance object

---
+++ advanceObj.js
@@ -0,0 +1,9 @@
+const student = {
+    name: "Adnan",
+    money: 5000,
+    study: "Web Programming",
+    subjects: ['wordpress', 'javascript', 'React', 'mongo', 'node', 'front end', 'full stack'],
+    exam: function () {
+        return this.name + 'participating in an exam'
+    }
+}
