6acab0d3f2bd35e0706b79b4b853f267d30338f5
jradnan <mohd.yasin085208@gmail.com>
2023-12-02 11:19:31 +0600

    invoke

---
+++ regexpression.js
@@ -0,0 +1,7 @@
+const text = "I am a web developer, developer, developer, hello";
+
+const i = text.replace(/developer/i, "programmer");
+const g = text.replace(/developer/g, "programmer");
+const m = text.match("o")
+
+console.log(m);
